cfdfc510d5925c007c6e1218d3842f77e0c90d1a
Big-Bad-Bird <hugo.morneau@cpe.fr>
2020-05-15 14:41:17 +0200

    crossorigin (partiel)

--- ASI_3_FrontEnd/src/main/java/CardShop/controllers/CardShopStaticController.java
+++ ASI_3_FrontEnd/src/main/java/CardShop/controllers/CardShopStaticController.java
@@ -3,9 +3,10 @@ package CardShop.controllers;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@CrossOrigin
 @Controller
 public class CardShopStaticController {
 
