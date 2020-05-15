c8622be99a02ca6bb7f5c2bc045b43fb78c42485
Gyrok <45562652+Gyrok@users.noreply.github.com>
2020-05-15 16:53:26 +0200

    Revert "yioiouh"
    This reverts commit 668b4d56a99002ae10bda1a4ee686583705e0390.

---
+++ ASI_3 Card/src/main/java/CardShop/cardService/CardShopCardController.java
@@ -0,0 +1,51 @@
+package CardShop.cardService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class CardShopCardController {
+	@Autowired
+	private CardShopCardService cardShopCardService;
+	
+	@RequestMapping("/Card/getAllCards")
+	public List<CardModel> getAllCards() {
+		return cardShopCardService.getallCard();
+	}
+		
+	@RequestMapping("/ById/{id}") // syntaxe pour ça: /Byid/id
+	public CardModel getCardById(@PathVariable String id) {
+		return cardShopCardService.getCardById(id);
+	}
+	
+	@RequestMapping("/ByName/{name}") // syntaxe pour ça: /ByName/name
+	public CardModel getCard(@PathVariable String name) {
+		return cardShopCardService.getCard(name);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/addCard")
+	public void addCard(@RequestBody CardModel card) {
+		cardShopCardService.addCard(card);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE,value="/delCard/{card}")
+	public void delCard(@PathVariable CardModel card) {
+		cardShopCardService.delCard(card);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT,value="/transaction")
+	public void buyCard(@RequestBody Transactor transactor) { 
+		cardShopCardService.transactionCard(transactor);
+	}
+
+}
+
+
---
+++ ASI_3 Card/src/main/java/CardShop/cardService/Transactor.java
@@ -0,0 +1,13 @@
+package CardShop.cardService;
+
+
+public class Transactor {
+
+	String cardId;
+	User cardSeller;
+	User cardBuyer;
+	int cost;
+	public Transactor() {
+		// TODO Auto-generated constructor stub
+	}
+}
