12a9ed0088e38c048f203d43ff890894f96d10a8
Gyrok <45562652+Gyrok@users.noreply.github.com>
2020-05-30 15:16:53 +0200

    sd

--- ASI_3_Market/src/main/java/Market/marketService/marketController.java
+++ ASI_3_Market/src/main/java/Market/marketService/marketController.java
@@ -1,9 +1,21 @@
 package Market.marketService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin
+@RestController
 public class marketController {
-
-	public marketController() {
-		// TODO Auto-generated constructor stub
+	
+	@Autowired
+	private marketService market;
+	
+	// Pour debug si prb 404
+	@RequestMapping("/market/hello")
+	public String hello() {
+		return "hello";
 	}
-
 }
